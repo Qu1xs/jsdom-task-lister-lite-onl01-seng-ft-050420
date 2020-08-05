f7b0f3de236f1c663deb99e5ac4b2bca5736c280
qu1xs <kevinnealmurphy@gmail.com>
2020-08-05 14:15:21 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const taskForm = document.getElementById("create-task-form");
+const taskDescription = document.getElementById("new-task-description");
+// const taskPriority = document.getElementById("new-task-priority");
+// const taskFilter = document.getElementById("new-task-filter");
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  taskForm.addEventListener("submit", (e) => {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(taskInput.value));
+    e.preventDefault();
+  });
 });
